605d8511d0f6c872f99dff7f0d5bfa3f54fae282
sandrockman <vhaskin1@stumail.jccc.edu>
2015-08-29 11:24:51 -0500

    Assets added, not working right.
    Forgot that a coroutine needs to end. Need to add it elsewhere. Not
    getting the collisions to work. Been having trouble sending this commit.
    Errors been throwing out without the gitignore file.  add new package.
    Call me if any developments at 620-228-3041 because I need to go to
    work. I'll have tonight and tomorrow to work on this till its done.

---
+++ AroundTheWorld/Assets/Scripts/ScriptCollisionShip.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptCollisionShip : MonoBehaviour {
+
+	public ParticleSystem explosion;
+	public AudioClip explodeSound;
+	//GameObject parentObject = transform.parent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		AudioSource.PlayClipAtPoint(explodeSound, transform.position);
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
